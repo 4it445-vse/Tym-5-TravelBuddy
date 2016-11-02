6d3426abb22efb75f83f954fb4e5501fbcb97044
Daniel Nejezchleb <dnn861@gmail.com>
2016-11-03 00:38:08 +0100

    test

--- frontend/src/components/LoginComponent/LoginComponent.js
+++ frontend/src/components/LoginComponent/LoginComponent.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export class LoginComponent extends Component{
   render(){
     return(
-
+//test
       <form className="form-inline">
           <input id="loginEmail" className="form-control" type="email" autoComplete="off" placeholder="e-mail"></input>
           <input id="loginPass" className="form-control" type="password" autoComplete="off" placeholder="password"></input>
