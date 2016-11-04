385fae1494aa02174ddd304cb1b8f47ca934e3f9
Daniel Nejezchleb <dnn861@gmail.com>
2016-11-04 10:04:07 +0100

    login form with basic functionality

--- frontend/src/components/LoginComponent/LoginComponent.js
+++ frontend/src/components/LoginComponent/LoginComponent.js
@@ -1,16 +1,105 @@
 import React, { Component } from 'react';
-
+import { FormGroup, ControlLabel, FormControl, HelpBlock, Form } from 'react-bootstrap';
+import api from '../../api.js';
 
 export class LoginComponent extends Component{
-  render(){
-    return(
-//test
-      <form className="form-inline">
-          <input id="loginEmail" className="form-control" type="email" autoComplete="off" placeholder="e-mail"></input>
-          <input id="loginPass" className="form-control" type="password" autoComplete="off" placeholder="password"></input>
-        <button type="submit" className="btn btn-default">Login</button>
-      </form>
+  constructor(props){
+    super(props);
 
+    this.state = {
+      loginEmail : "",
+      loginPassword : ""
+    }
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEmailChange(event) {
+      this.setState({loginEmail: event.target.value});
+  }
+
+  handlePasswordChange(event) {
+        this.setState({loginPassword: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('--- submitted: ', event.target);
+    console.log("email: ", this.state.loginEmail);
+    console.log("pwd: ", this.state.loginPassword);
+
+    var email = this.state.loginEmail;
+    var pwd = this.state.loginPassword;
+
+    if (pwd==="" || email===""){
+      console.log("empty values");
+      return;
+    }
+
+    api('')
+    .then(function(response) {
+      console.log(response.data);
+      console.log(response.status);
+      console.log(response.statusText);
+      console.log(response.headers);
+      console.log(response.config);
+    })
+      .catch(function (error) {
+        console.log("Errorx: ", error);
+      });
+
+
+  }
+
+  createField(type, key, desc,) {
+    switch (type) {
+      case 'password':
+      return (
+        <FormControl type={type} name={key} placeholder={type} value={this.state.loginPassword} onChange={this.handlePasswordChange}/>
+      );
+      case 'email':
+      return (
+        <FormControl type={type} name={key} placeholder={type} value={this.state.loginEmail} onChange={this.handleEmailChange}/>
+      );
+
+      default:
+        return;
+    }
+  }
+
+
+  render(){
+    const fields = [
+      /*key, label, type, desc*/
+      ['email', 'Email', 'email', ''],
+      ['password', 'Password', 'password', '']
+    ];
+
+
+    return(
+      <div className="login">
+        <Form inline onSubmit={this.handleSubmit} >
+
+        {fields.map(([key, label, type, desc]) => {
+
+            return (
+              <div key={key} className="login-form-element">
+                <FormGroup key={key} controlId={key} >
+{/*                 <ControlLabel>{label}</ControlLabel> */}
+                  {' '}
+                  {this.createField(type, key, desc)}
+                  </FormGroup>
+              </div>
+            );
+        })}
+
+          <div className="login-form-element">
+            <button type="submit" className="btn btn-default">Login</button>
+          </div>
+        </Form>
+      </div>
     );
   }
 }
