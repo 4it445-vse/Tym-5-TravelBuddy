2c46a1ca710827c8de3c10d34f77d0e0c97d01f7
Jakub Káňa <jakubkana@gmail.com>
2016-11-06 11:32:38 +0100

    - Login Component

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,23 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { About } from '../components/About/About.js';
-import { PageHeader } from '../components/PageHeader/PageHeader';
-import { PageFooter } from '../components/PageFooter/PageFooter';
 
-class LandingPage extends Component {
-    render() {
-        return (
-            <div className="container">
-                  <About/>
-                {/*<TopNavigation/>*/}
-                <PageFooter/>
-            </div>
-        );
-    }
-}
-
-export default LandingPage;
-=======
 import {PageHeader} from '../components/LandingPage/PageHeader/PageHeader';
 import {PageFooter} from '../components/LandingPage/PageFooter/PageFooter';
 import {About} from '../components/About/About';
@@ -34,4 +16,3 @@ export class LandingPage extends Component {
     );
   };
 }
->>>>>>> 9a3300b2a77cffed6df4e0047e8affdf03fa5fe0
