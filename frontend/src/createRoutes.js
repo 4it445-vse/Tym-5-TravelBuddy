4e94a98c45040d8f15bd2e1d0027a0f26e2dcb4a
Martin Šmíd <europanus@seznam.cz>
2016-11-06 12:22:03 +0100

    - added VerifiedPage.js as landing page for verified users

--- frontend/src/createRoutes.js
+++ frontend/src/createRoutes.js
@@ -4,6 +4,7 @@ import { IndexRoute, Route } from 'react-router';
 import { AppPage } from './pages/AppPage.js';
 import { LandingPage } from './pages/LandingPage.js';
 import { RegistrationPage } from './pages/RegistrationPage.js';
+import { VerifiedPage } from './pages/VerifiedPage.js';
 import { NoMatchPage } from './pages/NoMatchPage.js';
 
 export function createRoutes() {
@@ -11,6 +12,7 @@ export function createRoutes() {
     <Route path="/" component={AppPage}>
       <IndexRoute component={LandingPage}/>
       <Route path="/registration" component={RegistrationPage}/>
+      <Route path="/verified" component={VerifiedPage}/>
       <Route path="*" component={NoMatchPage}/>
     </Route>
   );
