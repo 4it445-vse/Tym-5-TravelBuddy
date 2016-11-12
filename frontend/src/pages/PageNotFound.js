1d6adf5288bdc6c94c06915d6c4e58076f7e3a0e
Jakub Káňa <jakubkana@gmail.com>
2016-11-12 19:27:24 +0100

    - PageNotFound

--- frontend/src/pages/PageNotFound.js
+++ frontend/src/pages/PageNotFound.js
@@ -4,10 +4,10 @@ import { PageFooter} from '../components/common/PageFooter/PageFooter';
 export class PageNotFound extends Component {
   render() {
     return (
-        <div className="ident-top-more">
+        <div className="ident-top">
             <div className="container">
                 <div className="row">
-                    <div className="col-*-8  col-*-offset-2">
+                    <div className="col-*-8 col-*-offset-2">
                         <div className="alert alert-warning">
                             <div className="h4">
                                 Page not Found
