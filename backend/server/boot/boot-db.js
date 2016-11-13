deda1eb7e51f308c90c551127a9befc66e191766
Martin Šmíd <europanus@seznam.cz>
2016-11-14 00:23:10 +0100

    - db model is booted and autoupdated

--- backend/server/boot/boot-db.js
+++ backend/server/boot/boot-db.js
@@ -1,46 +1,14 @@
 'use strict';
 
-// module.exports = function(app) {
-//   app.dataSources.mysqlds.autoupdate('UserMain', function(err) {
-//     const { UserMain } = app.models;
-//     if (!UserMain) { return; }
-//
-//     UserMain.count({}, function(err, count) {
-//       if (count !== 0) { return };
-//
-//       UserMain.create([
-//         {
-//           firstName: 'Rocky',
-//           lastName: 'Balboa',
-//           email: 'rocky@balboa.com',
-//           password: 'rocky123'
-//         }
-//       ], function(err, users) {
-//         if (err) throw err;
-//
-//         console.log('Models created: \n', users);
-//       });
-//     });
-//   });
-//
-//   app.dataSources.mysqlds.autoupdate('UserDetail', function(err) {
-//
-//     const { UserDetail } = app.models;
-//     if (!UserDetail) { return; }
-//
-//     UserDetail.count({}, function(err, count) {
-//       if (count !== 0) { return };
-//
-//     });
-//   });
-//
-// };
-
 module.exports = function(app) {
     var path = require('path');
     var models = require(path.resolve(__dirname, '../model-config.json'));
     var datasources = require(path.resolve(__dirname, '../datasources.json'));
 
+    // autoMigrateAll();
+    autoUpdateAll();
+
+    //autoupdate preserves data
     function autoUpdateAll(){
         Object.keys(models).forEach(function(key) {
             if (typeof models[key].dataSource != 'undefined') {
@@ -54,6 +22,7 @@ module.exports = function(app) {
         });
     }
 
+    //automigrate drops all tables and recreate them
     function autoMigrateAll(){
         Object.keys(models).forEach(function(key) {
             if (typeof models[key].dataSource != 'undefined') {
@@ -68,10 +37,3 @@ module.exports = function(app) {
     }
 
 };
-
-
-  // We dont want to create built-in User table yet
-  // app.dataSources.mysqlds.autoupdate('User', function(err) {
-  //   const { User } = app.models;
-  //   if (!User) { return; }
-  // });
