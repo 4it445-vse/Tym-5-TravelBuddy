d04eb7ad129062b9c5ac06b579e7cabd5e7ac37e
Martin Šmíd <europanus@seznam.cz>
2016-11-14 00:29:11 +0100

    - fixed dummy user booting

--- backend/server/boot/create-user.js
+++ backend/server/boot/create-user.js
@@ -4,17 +4,19 @@ module.exports = function(app) {
     const { UserMain } = app.models;
     if (!UserMain) { return; }
 
-    UserMain.create([
-      {
-        firstName: 'Rocky',
-        lastName: 'Balboa',
-        email: 'rocky@balboa.com',
-        password: 'rocky123'
-      }
-    ], function(err, users) {
-      if (err) throw err;
-
-      console.log('Models created: \n', users);
+    UserMain.count({}, function(err, count) {
+      if (count !== 0) { return; }
     });
-
+    // UserMain.create([
+    //   {
+    //     firstName: 'Rocky',
+    //     lastName: 'Balboa',
+    //     email: 'rocky@balboa.com',
+    //     password: 'rocky123'
+    //   }
+    // ], function(err, users) {
+    //   if (err) throw err;
+    //
+    //   console.log('Models created: \n', users);
+    // });
 };
