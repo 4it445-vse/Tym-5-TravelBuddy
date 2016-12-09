46534db7bf0a5337e97e4a0051b86bf4283e70ef
Daniel Nejezchleb <dnn861@gmail.com>
2016-12-09 18:44:13 +0100

    reset edit profile

--- frontend/src/components/ProfilePage/EditProfile.js
+++ frontend/src/components/ProfilePage/EditProfile.js
@@ -54,7 +54,7 @@ export class EditProfile extends Component {
         this.loadUserCountry = this.loadUserCountry.bind(this);
         this.loadUserMain();
         this.loadUserDetail();
-
+        this.loadUserCountry();
 
     }
 
@@ -144,17 +144,13 @@ export class EditProfile extends Component {
                         oldProfilePicture: response.data[keys[i]]
                     })
                   }
-                  else if (i==6)
-                  {
-                    this.loadUserCountry(response.data[keys[i]]);
-                  }
                   else
                   {
                     this.setState({
                         [keys[i]]: response.data[keys[i]]
                     })
                   }
-              }
+                }
             }
         }).catch((error) => {
             console.log("Error: ", error);
@@ -162,18 +158,18 @@ export class EditProfile extends Component {
         });
     }
 
-    loadUserCountry(recCountry) {
-        const srvUrl = '/Countries/' + recCountry + '?access_token=' + localStorage.accessToken;
-          console.log('srvUrl',srvUrl);
+    loadUserCountry() {
+        const srvUrl = '/Countries/' + this.state.refCountryId;
         api.get(srvUrl).then((response) => {
             if (response.status === 200) {
                 var keys = ["id"];
                 for (let i = 0; i < keys.length; i++) {
                     this.setState({
-                        country: response.data[keys[i]]
+                        countryID: response.data[0][keys[i]]
                     })
                 }
-              }
+                  console.log('countdssa',this.state.country);
+                  }
 
         }).catch((error) => {
             console.log("Error: ", error);
@@ -241,6 +237,7 @@ export class EditProfile extends Component {
     handleCountryChange(event) {
         this.setState({country: event.target.value});
     }
+    //zacatek obrazku
 
     handlePictureChange(event) {
         event.preventDefault();
@@ -263,12 +260,14 @@ export class EditProfile extends Component {
 
     setPicture(pictureBLOB, profilePicture) {
         this.setState({pictureBLOB: pictureBLOB, profilePicture: profilePicture});
+        //console.log('priflePictueLog',profilePicture);
     }
 
     handleInputChange(event) {
         this.setState({
             [event.target.name]: event.target.value
         });
+        console.log('--- state', this.state);
     }
 
     createPopover(text) {
@@ -281,6 +280,8 @@ export class EditProfile extends Component {
             </Popover>
         );
     }
+
+    //konec obrazku
 
     createField(type, key, desc, values) {
 
@@ -347,13 +348,8 @@ export class EditProfile extends Component {
                 return (<FormControl className={cssStyle} type={type} name={key} componentClass={type} value={this.state[key]} onChange={this.handleInputChange} />);
             case "select":
                 return (
-<<<<<<< HEAD
                     <FormControl className={cssStyle} componentClass="select" placeholder="Select your country" value={this.state.countryID} onChange={this.handleCountryChange}>
                         <option value={this.state[key]}></option>
-=======
-                    <FormControl className={cssClass} componentClass="select" placeholder="Select your country" value={this.state.country} onChange={this.handleCountryChange}>
-                        <option value=""></option>
->>>>>>> Oprava_editProdileCountry
                         {this.state.countries.map((element) => {
                             return (
                                 <option value={element.id} key={element.id}>{element.name}</option>
@@ -385,6 +381,34 @@ export class EditProfile extends Component {
         }
     }
 
+  /*  setActive(value) {
+      const srvUrl = '/UserMain/me?access_token=' + localStorage.accessToken;
+      var editActive = '';
+
+
+      if (this.state.isActive === null || this.state.isActive === 0)
+      {
+        this.setState({isActive: 1})
+      }
+      else {
+          this.setState({isActive: 0})
+      }
+
+      let formDataActive = this.getFormDataActive();
+      console.log("getFormDataActive",formDataActive);
+
+      api.patch(srvUrl, formDataActive).then(response => {
+          console.log('--- post usermain ok');
+          console.log('isActiveOK', value);
+      }).catch((error) => {
+          console.log('isActiveFail', value);
+      });
+      console.log('srvUrl',srvUrl);
+
+    }*/
+
+
+
     handleSubmit(event) {
         event.preventDefault();
         let errors = {};
@@ -392,6 +416,8 @@ export class EditProfile extends Component {
         let userMainFailed = false;
         let userDetailFailed = false;
         this.setState({isLoading: true});
+        //this.uploadProfilePicture(formData);
+
 
         if (formData.isActive == 'Non-active')
         {
@@ -446,7 +472,7 @@ export class EditProfile extends Component {
 
     getFormData() {
         return {
-            refCountryId: this.state.country,
+            country: this.state.country,
             motto: this.state.motto,
             bio: this.state.bio,
             firstName: this.state.firstName,
@@ -566,7 +592,10 @@ export class EditProfile extends Component {
                                             : errorMsg}</HelpBlock>
                                 </FormGroup>
                             );
-                        })}  <SubmitButton name="Save changes!" bsStyle="primary" isLoading={isLoading}/>
+                        })}
+                      {//<SubmitButton type="submit" bsStyle="primary">Save changes</Button>
+                    }
+                          <SubmitButton name="Save changes!" bsStyle="primary" isLoading={isLoading}/>
                     </form>
                 </div>
             </div>
