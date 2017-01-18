a946ca2c397c0c15a44fdb036f80608a0f7cfc7e
Jakub Káňa <jakubkana@gmail.com>
2017-01-18 22:08:54 +0100

    BuddyPage

--- frontend/src/components/BuddyPage/Profile/BuddyProfile.js
+++ frontend/src/components/BuddyPage/Profile/BuddyProfile.js
@@ -1,29 +1,51 @@
 import React, {Component} from 'react';
 import {Grid, Row, Col} from 'react-bootstrap';
-
+import api from '../../../api.js';
 
 export class BuddyProfile extends Component {
+
     constructor(props) {
         super(props);
-        console.log("USER DETAIL : ",this.props.user);
+
+        this.state = {
+            useritem: props.useritem
+        };
+        console.log("User PROPS",props.useritem);
     }
 
     render() {
+
+        if(!this.state.useritem) {
+
         return(
-           <div className="container">
-               <h1 className="text-center">Buddy Detail</h1>
-               <Grid bsClass="bg-info">
-                   <Row className="show-grid">
-                       <Col xs={4} md={4} lg={4}>Buddy:</Col>
-                       <Col xs={8} md={8} lg={8}></Col>
-                   </Row>
-                   <Row className="show-grid">
-                       <Col xs={4} md={4} lg={4}>Buddy:</Col>
-                       <Col xs={8} md={8} lg={8}></Col>
-                   </Row>
-               </Grid>
-           </div>
-        );
+
+                <div className="container">
+                    <div className="h1"></div>
+                    <br/>
+                    <h1 className="text-center">Buddy Detail</h1>
+                    <Grid>
+                        <Row className="show-grid">
+                            <Col xs={4} md={4} lg={4}>Name:</Col>
+                            <Col xs={8} md={8} lg={8}>userDetailData</Col>
+                        </Row>
+                        <Row className="show-grid">
+                            <Col xs={4} md={4} lg={4}>Buddy:</Col>
+                            <Col xs={8} md={8} lg={8}></Col>
+                        </Row>
+                    </Grid>
+                    <div className="container">
+                        <div className="h3">User Rating</div>
+                        <div>Rating 1.6</div>
+                    </div>
+                </div>
+        );}
+        else {
+           return (
+              <div className="container">
+                  <div className="h1 bg-danger text-center">No user has been found!</div>
+              </div>
+           ) ;
+            }
     }
 
 }
--- frontend/src/pages/BuddyPage.js
+++ frontend/src/pages/BuddyPage.js
@@ -11,57 +11,68 @@ import api from '../api.js';
 export class BuddyPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {userData: null,
-        userDetail:null};
+        this.state = {
+            userData: null,
+            useritem: null};
+
+
+        this.getUserDataById.bind(this);
+        this.getUserData.bind(this);
+        console.log("BuddyPage USERITEM:",this.state.useritem);
 }
-
-
-    componentDidMount() {
+    componentWillMount() {
         this.getUserData();
         this.getUserDataById(this.props.params.userId);
-        console.log("URL USER ID:",this.props.params.userId);
-    }
-
-    getUserData(){
-        api.get('UserMain/me?access_token=' + localStorage.accessToken)
-            .then((response) => {
-                console.log('--- getUserData', response.data);
-                this.setState({ userData: response.data });
-            })
-            .catch((error) => {
-                console.log("Error: ", error);
-            });
-
     }
 
     paramsForSearchTerm() {
         return {
             filter: {
-                include: 'userMain'
+                include: {relation: "userDetail"},
+                //{relation: "userDetail"}
             },
             limit: 1000
-        };
+        }
     }
-//, {params: this.paramsForSearchTerm()}
+
     getUserDataById(id) {
-        api.get('/UserDetail/'+id+'?access_token=' + localStorage.accessToken)
+        api.get('/UserMain/'+`${id}`+'?access_token='+localStorage.accessToken, {params: this.paramsForSearchTerm()})
             .then((response) => {
-                console.log('--- getUserDataDetail', response.data);
-                this.setState({ userDetail: response.data });
+                if (response.status === 200) {
+                     console.log('--- getUserDataDetail', response.data);
+                    this.setState({useritem: response.data});
+                }
             })
             .catch((error) => {
                 console.log("Error: ", error);
             });
     }
+
+    getUserData(){
+        api.get('UserMain/me?access_token=' + localStorage.accessToken)
+            .then((response) => {
+                if (response.status === 200) {
+                      console.log('--- getUserData', response.data);
+                    this.setState({userData: response.data});
+                }
+            })
+            .catch((error) => {
+                console.log("Error: ", error);
+            });
+
+    }
+
+
+
     render() {
-        const { userData } = this.state;
-        const { userDetail } = this.state;
+
+
         return (
         <div id="main-wrapper" className="homepage">
             <div className="gradient-wrapper">
-                <MainNavigation userData={userData}/>
+                <MainNavigation userData={this.state.userData}/>
                     <div className="container">
-                        <BuddyProfile user={userDetail}/>
+                        <BuddyProfile useritem={this.state.useritem}/>
                     </div>
                 <PageFooter/>
             </div>
